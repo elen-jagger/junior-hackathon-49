9cec8961e97bb11b917f894e5742aed9f2b0bb6a
Elena Leusik <elena.leusik@gmail.com>
2023-11-12 10:43:58 +0100

    feat: add "show route" button to menu

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -11,6 +11,8 @@ import { cellsToTiles, routeToTiles, swapTiles } from '../helpers/tiles';
 class GameScene extends Phaser.Scene {
   constructor() {
     super('Game');
+    this.initialPositionX = 135;
+    this.initialPositionY = 200;
   }
 
   preload() {
@@ -26,7 +28,11 @@ class GameScene extends Phaser.Scene {
   }
 
   addPlayer() {
-    const player = this.physics.add.sprite(135, 200, 'cat');
+    const player = this.physics.add.sprite(
+      this.initialPositionX,
+      this.initialPositionY,
+      'cat'
+    );
     player.setCollideWorldBounds(true);
 
     this.player = player;
@@ -83,32 +89,13 @@ class GameScene extends Phaser.Scene {
   create() {
     this.addPlayer();
 
-    // main menu buttons
-    const toStartBtn = this.add.text(100, 70, 'Go to start', {
-      fontFamily: 'Roboto',
-      fontSize: '30px',
-      color: '#086764',
-    });
-    toStartBtn.setInteractive().on('pointerdown', () => {
-      this.player.setX(135);
-      this.player.setY(200);
-    });
-
-    const restartBtn = this.add.text(400, 70, 'New map', {
-      fontFamily: 'Roboto',
-      fontSize: '30px',
-      color: '#086764',
-    });
-    restartBtn.setInteractive().on('pointerdown', () => {
-      this.scene.start('Game');
-    });
-
     // labyrinth
     const TILESIZE = 80;
     const cellsWidth = Math.floor(this.game.config.height / TILESIZE - 1);
     const cellsHeight = Math.floor(this.game.config.width / TILESIZE - 1);
     const columns = cellsWidth - 1;
     const rows = cellsHeight - 1;
+
     const x = this.game.config.width - TILESIZE * cellsHeight;
     const y = this.game.config.height - TILESIZE * cellsWidth + 40;
 
@@ -126,7 +113,39 @@ class GameScene extends Phaser.Scene {
 
     const route = routeToTiles(path, mazeCells);
 
-    this._renderLabyrinth(tiles, width, height, x, y, route);
+    this._renderLabyrinth(tiles, width, height, x, y);
+
+    // main menu buttons
+    const toStartBtn = this.add.text(100, 65, 'Go to start', {
+      fontFamily: 'Roboto',
+      fontSize: '30px',
+      color: '#086764',
+    });
+    toStartBtn.setInteractive().on('pointerdown', () => {
+      this.player.setX(this.initialPositionX);
+      this.player.setY(this.initialPositionY);
+    });
+
+    const restartBtn = this.add.text(400, 65, 'New map', {
+      fontFamily: 'Roboto',
+      fontSize: '30px',
+      color: '#086764',
+    });
+    restartBtn.setInteractive().on('pointerdown', () => {
+      this.scene.start('Game');
+    });
+
+    const showRouteBtn = this.add.text(700, 50, 'Show route\nand restart', {
+      fontFamily: 'Roboto',
+      fontSize: '30px',
+      color: '#086764',
+    });
+    showRouteBtn.setInteractive().on('pointerdown', () => {
+      this.showRoute(route, width, height, x, y);
+      setTimeout(() => {
+        this.scene.start('Game');
+      }, 2000);
+    });
 
     this.addFinish();
 
@@ -137,11 +156,22 @@ class GameScene extends Phaser.Scene {
     );
   }
 
+  showRoute(route, width, height, x, y) {
+    const routeMap = this.make.tilemap({
+      data: route,
+      tileWidth: 50,
+      tileHeight: 50,
+    });
+    const routeTiles = routeMap.addTilesetImage('route');
+    const routeLayer = routeMap.createLayer(0, routeTiles, x, y);
+    routeLayer.setDisplaySize(width, height);
+  }
+
   finishGame() {
     this.scene.start('EndScene');
   }
 
-  _renderLabyrinth(tiles, width, height, x, y, route) {
+  _renderLabyrinth(tiles, width, height, x, y) {
     const floorMap = this.make.tilemap({
       data: tiles,
       tileWidth: 50,
@@ -160,15 +190,6 @@ class GameScene extends Phaser.Scene {
     const wallTiles = wallsMap.addTilesetImage('ground');
     const wallsLayer = wallsMap.createLayer(0, wallTiles, x, y);
     wallsLayer.setDisplaySize(width, height);
-
-    const routeMap = this.make.tilemap({
-      data: route,
-      tileWidth: 50,
-      tileHeight: 50,
-    });
-    const routeTiles = routeMap.addTilesetImage('route');
-    const routeLayer = routeMap.createLayer(0, routeTiles, x, y);
-    routeLayer.setDisplaySize(width, height);
 
     wallsLayer.setCollisionBetween(1, 255);
     this.physics.add.collider(this.player, wallsLayer);
